fd171ec58bb7a8659bf23c226ac52bd9efc5e3e1
LaisTaine <lais.taine@gmail.com>
2025-07-22 12:58:04 -0300

    docs(auth): adiciona documentacao swagger ao LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({
+    description: 'E-mail de login do usuário.',
+    example: 'aluno@email.com',
+  })
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => value.trim().toLowerCase())
   email: string;
 
+  @ApiProperty({
+    description: 'Senha de acesso do usuário.',
+    example: 'senhaForte123',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
