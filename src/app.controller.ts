9c02470af7a11ff9f81989180f2db0dcd0bb8970
LaisTaine <lais.taine@gmail.com>
2025-07-22 15:42:32 -0300

    docs(app): adiciona documentacao ao health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { IsPublic } from './decorators/is-public';
 
+@ApiTags('Status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @IsPublic()
+  @ApiOperation({ summary: 'Verifica o status da API (Health Check)' }) 
+  @ApiResponse({ 
+    status: 200,
+    description: 'Retorna uma mensagem de boas-vindas confirmando que a API está no ar.',
+    type: String, 
+  })
   getHello(): string {
     return this.appService.getHello();
   }
