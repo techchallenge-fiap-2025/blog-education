893633fde40625db0231e09f229338c477ea14b9
Felipe Ragne Silveira <frsilveira01@outlook.com>
2025-08-14 19:08:16 -0300

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { LogginInterceptor } from './shared/interceptors/logging.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -22,8 +21,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  // app.useGlobalFilters(new HttpExceptionFilter());
+
   app.useGlobalInterceptors(new LogginInterceptor());
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap();
